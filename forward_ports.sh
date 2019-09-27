109d634dc890fbe6d7e2dd593340fd83ccabb8d3
root <root@terraform.europe-west3-a.c.absolute-router-242207.internal>
2019-09-27 17:05:40 +0000

    New files

--- forward_ports.sh
+++ forward_ports.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#  forward local ports  
+# Script to forward local ports to Aromory Spinnaker Deck and Gate pod. 
 
 if [ "$1" == "kill" ]; then
-  echo "killing kubectl port-forward ." 
+  echo "killing kubectl port-forward processes..." 
   pgrep kubectl | xargs kill
   exit
 fi
@@ -28,3 +28,4 @@ echo "prometheus: http://localhost:9090"
 ZIP_POD=$(kubectl -n istio-system get pod -l app=jaeger --kubeconfig=kubeconfig -ojsonpath='{.items[0].metadata.name}')
 kubectl -n istio-system port-forward ${ZIP_POD} 16686 --kubeconfig=kubeconfig >> /dev/null 2>&1 &
 echo "jaeger: http://localhost:16686"
+
--- templates/template-create-spinnaker.sh
+++ templates/template-create-spinnaker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-CORE_CLUSTER=gke-gevops
+CORE_CLUSTER=${cluster_name}
 CORE_USER=core-spinnaker-service-account
 KUBECONFIG_FILE=spinnaker-kubeconfig
 SPINNAKER_SA=create-spinnaker-sa.yaml
 
+# Create spinnaker kubeconfig file
 kubectl --kubeconfig=kubeconfig apply -f $SPINNAKER_SA
 CORE_TOKEN=$(kubectl  --kubeconfig=kubeconfig  get secret \
     $(kubectl --kubeconfig=kubeconfig get serviceaccount spinnaker-service-account \
@@ -13,8 +14,8 @@ CORE_TOKEN=$(kubectl  --kubeconfig=kubeconfig  get secret \
     -n spinnaker \
     -o jsonpath='{.data.token}' | base64 --decode)
 
-kubectl --kubeconfig=kubeconfig config view --raw -o json | jq -r '.clusters[] | select(.name == "'gke-gevops'") | .cluster."certificate-authority-data"' | base64 -d > core_cluster_ca.crt
-CORE_SERVER=$(kubectl --kubeconfig=kubeconfig config view --raw -o json | jq -r '.clusters[] | select(.name == "'gke-gevops'") | .cluster."server"')
+kubectl --kubeconfig=kubeconfig config view --raw -o json | jq -r '.clusters[] | select(.name == "'${cluster_name}'") | .cluster."certificate-authority-data"' | base64 -d > core_cluster_ca.crt
+CORE_SERVER=$(kubectl --kubeconfig=kubeconfig config view --raw -o json | jq -r '.clusters[] | select(.name == "'${cluster_name}'") | .cluster."server"')
 kubectl config --kubeconfig=$KUBECONFIG_FILE set-cluster $CORE_CLUSTER \
     --certificate-authority=./core_cluster_ca.crt \
     --embed-certs=true \
@@ -22,4 +23,7 @@ kubectl config --kubeconfig=$KUBECONFIG_FILE set-cluster $CORE_CLUSTER \
 
 kubectl config --kubeconfig=$KUBECONFIG_FILE set-credentials $CORE_USER --token $CORE_TOKEN
 kubectl config --kubeconfig=$KUBECONFIG_FILE set-context core --user $CORE_USER --cluster $CORE_CLUSTER
-kubectl --kubeconfig=kubeconfig create secret generic --from-file=./spinnaker-kubeconfig spin-kubeconfig -n spinnaker
+kubectl --kubeconfig=kubeconfig create secret generic --from-file=./spinnaker-kubeconfig --from-file=./gcp-service-account.json spin-secrets -n spinnaker
+
+# install spinaker
+kubectl --kubeconfig=kubeconfig apply -f haliard-install.yaml
